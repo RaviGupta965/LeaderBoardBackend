06b7c20520cb536193c2933848c6323c8e6dde1c
RaviGupta965 <clicktoravi1234@gmail.com>
2025-07-18 22:34:28 +0530

    env-configured

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.use(express.json());
 
 app.use('/api', userRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => app.listen(5000, () => console.log('Server running on port 5000')))
   .catch(err => console.error(err));
