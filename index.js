54019bd7b0fd5c964577ea0ce02fd6e36b8ec681
Ravi Gupta <133798565+RaviGupta965@users.noreply.github.com>
2025-07-18 23:29:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import userRoutes from './routes/userRoutes.js';
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 
 app.use('/api', userRoutes);
